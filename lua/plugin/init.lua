795aa4ddad3b9af77afc7c7949f04bb8a79990ac
Sam Jacobs <thischarmingsam@gmail.com>
2024-12-21 15:51:47 +0000

    Attempting to add command to switch to .scss files intead of tests

--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -1,84 +1,99 @@
 local M = {}
 
 local file_path_transformations = {
-    unchanged = function(path) return path end,
+	unchanged = function(path)
+		return path
+	end,
 
-    classToTemplate = function(path) return path:gsub("%.ts$", ".html") end,
-    templateToClass = function(path) return path:gsub("%.html$", ".ts") end,
+	classToTemplate = function(path)
+		return path:gsub("%.ts$", ".html")
+	end,
+	templateToClass = function(path)
+		return path:gsub("%.html$", ".ts")
+	end,
 
-    testToClass = function(path) return path:gsub("%.spec%.ts$", ".ts") end,
-    classToTest = function(path) return path:gsub("%.ts$", ".spec.ts") end,
+	stylesToClass = function(path)
+		return path:gsub("%.scss$", ".ts")
+	end,
+	classToStyles = function(path)
+		return path:gsub("%.ts$", ".scss")
+	end,
 
-    testToTemplate = function(path) return path:gsub("%.spec%.ts$", ".html") end,
-    templateToTest = function(path) return path:gsub("%.html$", ".spec.ts") end,
+	stylesToTemplate = function(path)
+		return path:gsub("%.scss$", ".html")
+	end,
+	templateToStyles = function(path)
+		return path:gsub("%.html$", ".scss")
+	end,
 }
 
 local function open_target_file(opts)
-    local relative_file_path = vim.fn.expand("%")
+	local relative_file_path = vim.fn.expand("%")
 
-    for _, transformation in ipairs(opts.file_path_transformation_map) do
-        if string.match(relative_file_path, transformation.regex) then
-            local file_to_open = transformation.transform(relative_file_path)
-            vim.cmd.edit(file_to_open)
-            return
-        end
-    end
+	for _, transformation in ipairs(opts.file_path_transformation_map) do
+		if string.match(relative_file_path, transformation.regex) then
+			local file_to_open = transformation.transform(relative_file_path)
+			vim.cmd.edit(file_to_open)
+			return
+		end
+	end
 
-    error(":" .. opts.command .. " could not determine target file", 1)
+	error(":" .. opts.command .. " could not determine target file", 1)
 end
 
-function M.quick_switch_toggle()
-    local file_path_transformation_map = {
-        { regex = "%.component%.ts$",   transform = file_path_transformations.classToTemplate },
-        { regex = "%.component%.html$", transform = file_path_transformations.templateToClass },
-        { regex = "%.spec%.ts$",        transform = file_path_transformations.testToClass },
-        { regex = "%.ts$",              transform = file_path_transformations.classToTest },
-    }
-
-    open_target_file({ file_path_transformation_map = file_path_transformation_map, command = "NgQuickSwitchToggle" })
-end
+-- function M.quick_switch_toggle()
+--     local file_path_transformation_map = {
+--         { regex = "%.component%.ts$",   transform = file_path_transformations.classToTemplate },
+--         { regex = "%.component%.html$", transform = file_path_transformations.templateToClass },
+--         { regex = "%.spec%.ts$",        transform = file_path_transformations.testToClass },
+--         { regex = "%.ts$",              transform = file_path_transformations.classToTest },
+--     }
+--
+--     open_target_file({ file_path_transformation_map = file_path_transformation_map, command = "NgQuickSwitchToggle" })
+-- end
 
 function M.quick_switch_class()
-    local file_path_transformation_map = {
-        { regex = "%.spec%.ts$", transform = file_path_transformations.testToClass },
-        { regex = "%.ts$",       transform = file_path_transformations.unchanged },
-        { regex = "%.html$",     transform = file_path_transformations.templateToClass },
-    }
+	local file_path_transformation_map = {
+		{ regex = "%.scss$", transform = file_path_transformations.stylesToClass },
+		{ regex = "%.ts$", transform = file_path_transformations.unchanged },
+		{ regex = "%.html$", transform = file_path_transformations.templateToClass },
+	}
 
-    open_target_file({ file_path_transformation_map = file_path_transformation_map, command = "NgQuickSwitchClass" })
+	open_target_file({ file_path_transformation_map = file_path_transformation_map, command = "NgQuickSwitchClass" })
 end
 
 function M.quick_switch_template()
-    local file_path_transformation_map = {
-        { regex = "%.spec%.ts$", transform = file_path_transformations.testToTemplate },
-        { regex = "%.ts$",       transform = file_path_transformations.classToTemplate },
-        { regex = "%.html$",     transform = file_path_transformations.unchanged },
-    }
+	local file_path_transformation_map = {
+		{ regex = "%.scss$", transform = file_path_transformations.stylesToTemplate },
+		{ regex = "%.ts$", transform = file_path_transformations.classToTemplate },
+		{ regex = "%.html$", transform = file_path_transformations.unchanged },
+	}
 
-    open_target_file({ file_path_transformation_map = file_path_transformation_map, command = "NgQuickSwitchTemplate" })
+	open_target_file({ file_path_transformation_map = file_path_transformation_map, command = "NgQuickSwitchTemplate" })
 end
 
-function M.quick_switch_test()
-    local file_path_transformation_map = {
-        { regex = "%.spec%.ts$", transform = file_path_transformations.unchanged },
-        { regex = "%.ts$",       transform = file_path_transformations.classToTest },
-        { regex = "%.html$",     transform = file_path_transformations.templateToTest },
-    }
+function M.quick_switch_styles()
+	local file_path_transformation_map = {
+		{ regex = "%.scss$", transform = file_path_transformations.unchanged },
+		{ regex = "%.ts$", transform = file_path_transformations.classToStyles },
+		{ regex = "%.html$", transform = file_path_transformations.templateToStyles },
+	}
 
-    open_target_file({ file_path_transformation_map = file_path_transformation_map, command = "NgQuickSwitchTest" })
+	open_target_file({ file_path_transformation_map = file_path_transformation_map, command = "NgQuickSwitchStyles" })
 end
 
 function M.setup(opts)
-    opts = opts or {}
+	opts = opts or {}
 
-    vim.api.nvim_create_user_command("NgQuickSwitchToggle", M.quick_switch_toggle, {})
-    vim.api.nvim_create_user_command("NgQuickSwitchComponent", M.quick_switch_class, {})
-    vim.api.nvim_create_user_command("NgQuickSwitchTemplate", M.quick_switch_template, {})
-    vim.api.nvim_create_user_command("NgQuickSwitchTest", M.quick_switch_test, {})
+	vim.api.nvim_create_user_command("NgQuickSwitchComponent", M.quick_switch_class, {})
+	vim.api.nvim_create_user_command("NgQuickSwitchTemplate", M.quick_switch_template, {})
+	vim.api.nvim_create_user_command("NgQuickSwitchStyles", M.quick_switch_styles, {})
 
-    if opts.use_default_keymaps then
-        vim.keymap.set("n", "<leader>qs", ":NgQuickSwitchToggle<cr>")
-    end
+	if opts.use_default_keymaps then
+		vim.keymap.set("n", "<leader>ac", ":NgQuickSwitchComponent<cr>")
+		vim.keymap.set("n", "<leader>as", ":NgQuickSwitchStyles<cr>")
+		vim.keymap.set("n", "<leader>at", ":NgQuickSwitchTemplate<cr>")
+	end
 end
 
 return M
